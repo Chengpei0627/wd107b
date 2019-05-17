fdfcd1677813eb7b3fa2c8265dab89ea6ea38215
Chengpei0627 <ab8627@gmail.com>
2019-05-17 11:27:48 +0800

    add many

---
+++ myproject.md
@@ -0,0 +1,10 @@
+# 我的專案
+
+
+## 1. 網誌系統
+
+連結：[blog](Finalexam/blog)
+
+## 2. 計算機
+
+連結：[calculator](Finalexam/Javascript)
---
+++ node/homework/work1.js
@@ -0,0 +1,8 @@
+var a = 1;
+b = 1;
+while (b<=10) {
+    a=a*b
+    b=b+1
+}
+console.log('a=', a);
+console.log('b=', b);
